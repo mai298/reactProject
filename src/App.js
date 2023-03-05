eb20a7d985bd7202fef36267389b2a888e56ed0d
maimohamed <memomai171@gmail.com>
2023-03-05 16:15:37 +0200

    changing in app.js and index.html

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          hii
+          new branch
         </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,8 +20,93 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <ul className="sub-ul sub-of-js">
+          <li>
+            <a href="">coming soon🔥</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </header>
+  <header className="show-when-mobile">
+    <h1>Courses 4 Arab</h1>
+    <label className="absolute" htmlFor="burger">
+      <i className="fas fa-bars" />
+    </label>
+    <input id="burger" type="checkbox" />
+    <div className="show-on-click">
+      <div className="main-div">
+        <label htmlFor="html">
+          HTML <i className="fas fa-plus" />{" "}
+        </label>
+        <input id="html" type="checkbox" />
+        <ul className="sub-div">
+          <li>
+            <a href="">Full Course</a>
+          </li>
+          <li>
+            <a href="">Crash Course</a>
+          </li>
+          <li>
+            <a href="">learn in 1h</a>
+          </li>
+        </ul>
+      </div>
+      <div className="main-div">
+        <label htmlFor="css">
+          CSS <i className="fas fa-plus" />{" "}
+        </label>
+        <input id="css" type="checkbox" />
+        <ul className="sub-div">
+          <li>
+            <a href="">Full Course</a>
+          </li>
+          <li>
+            <a href="">CSS Examples</a>
+          </li>
+          <li>
+            <label className="mini-projects" htmlFor="mini">
+              mini projects <i className="fas fa-plus" />{" "}
+            </label>
+            <input id="mini" type="checkbox" />
+            <ul className="sub-sub-div">
+              <li>
+                <a href="">project 1</a>
+              </li>
+              <li>
+                <a href="">project 2</a>
+              </li>
+              <li>
+                <a href="">project 3</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div className="main-div">
+        <label htmlFor="js">
+          JavaScript <i className="fas fa-plus" />{" "}
+        </label>
+        <input id="js" type="checkbox" />
+        <ul className="sub-div">
+          <li>
+            <a href="">coming soon🔥</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </header>
+  <main>
+    Respossive Drop-down <br />
+    Menu Bar <br />
+    HTML &amp; CSS only
+  </main>
+  <footer>
+    Designed and developed by Courses4Arab.com
+    <span>🧡</span>
+  </footer>
+</>
+
   );
 }
 
